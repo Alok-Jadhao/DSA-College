fc1019687d142c9beaf5bf3be93dac4e0201bd71
Alok Jadhao <144699874+Alok-Jadhao@users.noreply.github.com>
2024-09-13 12:02:35 +0530

    Update BST.c

--- BST.c
+++ BST.c
@@ -39,6 +39,110 @@ void inorderTraversal(Node* root) {
     }
 }
 
+void preorderTraversal(Node* root) {
+    if(root != NULL) {
+        printf("%d ", root->data);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+    }
+}
+
+void postorderTraversal(Node* root) {
+    if(root != NULL) {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+
+// Delete leaf and parent with one child.
+Node* deleteNode(Node* root, int data) {
+    if(root == NULL) {
+        return root;
+    }
+
+    if(data < root->data) {
+        root->left = deleteNode(root->left, data);
+    }
+    else if(data > root->data) {
+        root->right = deleteNode(root->right, data);
+    }
+    else {
+        if(root->left == NULL) {
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        }
+
+        if(root->right == NULL) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        
+    }
+    return root;
+    
+}
+
+
+int main() {
+    Node* root = NULL;
+    //user input
+    int n;
+    int data;
+    
+    while(1) {
+        printf("1. Insert  2. Delete  3. Traverse  4. Exit\n");
+        scanf("%d", &n);
+
+        switch(n) {
+            case 1:
+                printf("Enter the data: ");
+                scanf("%d", &data);
+                if(data != -1) {
+                    root = insertNode(root, data);    
+                }
+                
+                break;
+
+            case 2:
+                printf("Enter data to be deleted: ");
+                scanf("%d", &data);
+                deleteNode(root, data);
+                break;
+
+            case 3:
+                printf("Inorder Traversal: ");
+                inorderTraversal(root);
+                printf("\n");
+                printf("Preorder Traversal: ");
+                preorderTraversal(root);
+                printf("\n");
+                printf("Postorder Traversal: ");
+                postorderTraversal(root);
+                printf("\n");
+                break;
+
+            default:
+                printf("Invalid Input\n");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // void deleteNode(Node* root, int data) {
 //     if(root == NULL) {
 //         return;
@@ -50,13 +154,9 @@ void inorderTraversal(Node* root) {
 //     }
 // }
 
-Node* deleteNode(Node* root, int data) {
-    if(root == NULL) {
-        return root;
-    }
 
-    
-}
+
+
 
 // struct Node* delNode(struct Node* root, int x) {
 
@@ -94,7 +194,3 @@ Node* deleteNode(Node* root, int data) {
 //     }
 //     return root;
 // }
-
-
-
-
